1d617281fd18b3251f323d1e702d2d77c85a1305
DICHHA RAI <dichha.chamling@gmail.com>
2018-12-11 12:37:12 -0800

    made the custom filter tag recursive to handle more than one <code> tag

--- conf.py
+++ conf.py
@@ -236,28 +236,34 @@ htmlhelp_basename = 'PythonCoursewareProjectdoc'
 # For custom jinja2 filter
 import jinja2
 
-#
 def extractText(blob):
     if "<code" in blob:
-        title = ""
-        idx0 = blob.find("<code")
-
-        firstSub = blob[0:idx0]
-        title += firstSub
-
-        idx1 = blob.find("\"pre\">")
-        idx2 = blob.find("</span>")
-        secondSub = blob[idx1+6:idx2]
-        title += secondSub
-        title += " "
-
-        idx3 = blob.find("</code>")
-        thirdSub = blob[idx3+7:]
-        title += thirdSub
-
-        return title
+        return extractTextHelper("", blob)
 
     return blob
+
+def extractTextHelper(title, blob):
+    if "<code" not in blob:
+        title += blob
+        return title
+
+
+    idx0 = blob.find("<code")
+
+    firstSub = blob[0:idx0]
+    title += firstSub
+
+    idx1 = blob.find("\"pre\">")
+    idx2 = blob.find("</span>")
+    secondSub = blob[idx1+6:idx2]
+    title += secondSub
+    title += " "
+
+    idx3 = blob.find("</code>")
+
+    thirdSub = blob[idx3+7:]
+
+    return extractTextHelper(title, thirdSub)
 
 
 def extractTextII(blob):
